c2bcdf9b1281c4890d834c350d687e4029e0ab67
Olesia Ivanova <ivanova.ov_87@mail.ru>
2022-08-07 09:08:02 +0300

    первый коммит шестого урока

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use srtict';
 
+function one(x) {
+    function two() {
+        const a = +prompt("Угадай число от 1 до 100");
+        x--;
+
+        if (a > x) {
+            console.log('Загаданное число меньше');
+        } else if (a < x) {
+            console.log('Загаданное число больше');
+        } else if (a === !isNaN) {
+            a = +prompt("Введите число");
+        } else if () {
+            alert("Поздравляю, Вы угадали!!!");
+        } else {
+            alert("Игра окончена");
+        }
+
+    }
+    two();
+}
+one(100);
